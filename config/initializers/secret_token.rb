e5c56d5591177b12df2750fcfaedafdd22aed9a3
Hieu <ptrhieu.it@gmail.com>
2013-06-28 00:16:06 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venshop::Application.config.secret_token = '076bd5e1fb598bb6c27039dd662e1d21b79c55ef73114d61946b326953374fb2bdd24d98246e2c77c00d1317d5c6fc0359571790ef7f7bc0f91dcca6357f24e1'
